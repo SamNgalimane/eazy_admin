324ef58fbbbd6f6d3e286057737db5a3a4513dac
SamNgalimane <68399804+SamNgalimane@users.noreply.github.com>
2021-12-13 19:06:19 +0200

    Update App.js
    Update the sub heading of the app to "Hello DSK Developers"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Hello DSK Developers</span>
         </p>
         <a
           className="App-link"
